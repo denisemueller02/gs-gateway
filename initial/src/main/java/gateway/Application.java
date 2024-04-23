0f8d303b05eb7b8396891383e7c97c879385683f
denisemueller02 <146842156+denisemueller02@users.noreply.github.com>
2024-04-23 21:04:36 +0200

    inital commit

--- initial/src/main/java/gateway/Application.java
+++ initial/src/main/java/gateway/Application.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
 }

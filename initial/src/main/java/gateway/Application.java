365dc81123979e2f1a6f919ad7de0e7d61d70ffa
Cindy Sebastian <cindy.sebastian@gmx.net>
2024-04-25 16:35:20 +0200

    test fixed fr fr

--- initial/src/main/java/gateway/Application.java
+++ initial/src/main/java/gateway/Application.java
@@ -2,6 +2,8 @@ package gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
